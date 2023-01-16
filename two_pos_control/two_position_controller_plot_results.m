90751931d5c896f8a19edf4f9e638524e0e5a310
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-01-16 14:21:10 +0100

    delete unused files

--- two_pos_control/two_position_controller_plot_results.m
+++ two_pos_control/two_position_controller_plot_results.m
@@ -1,11 +1,11 @@
-data_filename = '';
+data_filename = 'data_two_position_controller_20230110-173611.txt';
 temperature = load(data_filename);
 dt = 1; %sample time
 samples = length(temperature);
 t = (0:samples-1)*dt;
 
-set_point = 26;
-histeresis = 0.5;
+set_point = 27.5;
+histeresis = 0.3;
 
 figure;
 plot(t,temperature, 'b.');
