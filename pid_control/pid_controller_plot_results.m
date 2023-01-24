6945cc6849706ea9a7cc366384d4949feff47dad
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-01-24 18:09:16 +0100

    Implement working PID regulator

--- pid_control/pid_controller_plot_results.m
+++ pid_control/pid_controller_plot_results.m
@@ -1,16 +1,27 @@
-data_filename = '';
+data_filename = 'pid_measurements.txt';
+data_filename_2 = 'u_measurements.txt';
 temperature = load(data_filename);
+u = load(data_filename_2);
 dt = 1;
 number_of_samples = length(temperature);
 t = (0:number_of_samples-1)*dt;
 set_point = 27.5;
 
-figure(1);
+
+subplot(2,1,1);
 plot(t,temperature, 'b.');
 hold on;
 line([0 t(end)],[set_point, set_point],'Color','red','LineStyle','--');
 hold off;
-title('PI controller, Kp=1.2, Ki=0.002, Kd=0');
+title('PI controller, Kp=0.6073, Ki=0.0024, Kd=0');
+xlabel('Time (s)');
+ylabel('Temperature (C)');
+legend('measurement samples');
+axis tight;
+grid minor;
+subplot(2,1,2);
+title('PI controller, Kp=0.6073, Ki=0.0024, Kd=0');
+plot(t, u, 'b.');
 xlabel('Time (s)');
 ylabel('Temperature (C)');
 legend('measurement samples');
